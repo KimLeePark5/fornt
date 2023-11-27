cbdb35d694e4f3cc3d38cf545b5d27073123408e
kingnongmeel <125981939+kingnongmeel@users.noreply.github.com>
2023-11-27 22:40:38 +0900

    #4-1127-고객리스트조회

--- thesilver/src/App.js
+++ thesilver/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Temp from "./pages/Temp";
+import Customers from "./pages/Customers";
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={ <Temp/> }></Route>
+            <Route path="/customers" element={ <Customers/> }></Route>
         </Routes>
     </BrowserRouter>
   );
