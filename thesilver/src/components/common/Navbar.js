336fd0fba3b198e2eeba9112d2c98b78347362bf
kingnongmeel <125981939+kingnongmeel@users.noreply.github.com>
2023-12-04 23:46:16 +0900

    #4-1127-고객리스트조회

--- thesilver/src/components/common/Navbar.js
+++ thesilver/src/components/common/Navbar.js
@@ -64,29 +64,21 @@ function Navbar() {
                         <li><NavLink to="/myAttend">근태 관리</NavLink></li>
                         <li><NavLink to="">연차 관리</NavLink></li>
                     </ul>
-                    )}
-                 </li>
-                <li>
-                    <a className="Menu" onClick={() => onClickMenuHandler("employee")}>직원 관리</a>
-                    {isSubMenuOpen.employee && (
-                    <ul className="subMenu">
-                        <li><NavLink to="/attend-management">직원 근태 관리</NavLink></li>
-                        <li><NavLink to="/employees">직원 정보 관리</NavLink></li>
-                        <li><NavLink to="">직원 연차 관리</NavLink></li>
-                    </ul>
-                    )}
-
                 </li>
                 <ProtectedRoute onlyAdminMaster={true}>
                     <li>
-                        <a className={`Menu`} onClick={() => onClickMenuHandler("employee")}>직원 관리</a>
-                        <ul className={`subMenu ${isSubMenuOpen.employee ? 'active' : ''}`}>
-                            <li><NavLink to="/employees">직원 정보 관리</NavLink></li>
-                            <li><NavLink to="/attend-management">직원 근태 관리</NavLink></li>
-                            <li><NavLink to="">직원 연차 관리</NavLink></li>
-                        </ul>
+                        <a className="Menu" onClick={() => onClickMenuHandler("employee")}>직원 관리</a>
+                        {isSubMenuOpen.employee && (
+                            <ul className="subMenu">
+                                <li><NavLink to="/attend-management">직원 근태 관리</NavLink></li>
+                                <li><NavLink to="/employees">직원 정보 관리</NavLink></li>
+                                <li><NavLink to="">직원 연차 관리</NavLink></li>
+                            </ul>
+                        )}
+
                     </li>
                 </ProtectedRoute>
+
                 <li>
                     <a className={`Menu`} onClick={() => onClickMenuHandler("myInfo")}>내정보 관리</a>
                     <ul className={`subMenu ${isSubMenuOpen.myInfo ? 'active' : ''}`}>
